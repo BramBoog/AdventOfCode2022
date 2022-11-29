a8a7ff6ad8922f6b324f45d744c78d9508393846
Bram Boogaarts <bram.boogaarts@gmail.com>
2022-11-29 16:05:31 +0100

    added test file

---
+++ src/test/scala/TestSolutions.scala
@@ -0,0 +1,8 @@
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestSolutions extends AnyFunSuite:
+  ???
+//   test("Day01") {
+//     assertResult()(actual = Day01.answer1)
+//     assertResult()(actual = Day01.answer2)
+//   }
