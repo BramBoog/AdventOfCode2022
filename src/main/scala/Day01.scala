221cf55224caf4fabcdb18ef8e25d9905548e470
BramBoog <93602108+BramBoog@users.noreply.github.com>
2022-12-02 10:07:24 +0100

    Day01 (#1)
    * Day01
    
    * refactored to only list and string functions
    
    * takeRight(3) instead of reverse.take(3)

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,4 +1,14 @@
 object Day01 extends App:
-  import scala.io.Source
+    import scala.io.Source
 
-  val instructions: List[String] = Source.fromResource("InputDay01.txt")
+    val caloriesPerElf: List[Int] =
+        Source.fromResource("InputDay01.txt").mkString
+        .split("\r\n\r\n").toList.map(_.split("\r\n").toList.map(_.toInt)).map(_.sum)
+    
+    // Part 1  
+    val answer1: Int = caloriesPerElf.max
+    println(s"Result part 1: ${answer1}")
+
+    // Part 2
+    val answer2: Int = caloriesPerElf.sorted.takeRight(3).sum
+    println(s"Resutl part 2: ${answer2}")
