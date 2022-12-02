68c0420b6cef6171c4544e78da6c57090d0b0f40
Bram Boogaarts <bram.boogaarts@gmail.com>
2022-12-02 09:54:08 +0100

    refactored to only list and string functions

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,25 +1,14 @@
 object Day01 extends App:
     import scala.io.Source
 
-    val calories: List[String] = Source.fromResource("InputDay01.txt").getLines.toList
-
-    def countUpCalories(inputList: List[String], groupedCals: List[Int], curGroup: Int = 0): List[Int] =
-        inputList.headOption match
-            case None => groupedCals
-            case Some("") => countUpCalories(inputList.tail, groupedCals.appended(curGroup))
-            case Some(x) if x.toIntOption.isDefined => countUpCalories(inputList.tail, groupedCals, curGroup + x.toInt)
-            case _ => sys.error(s"Cannot convert input: ${inputList.head} to Int.")
-
-    val groupedCalories: List[Int] = countUpCalories(calories, List())
-
+    val caloriesPerElf: List[Int] =
+        Source.fromResource("InputDay01.txt").mkString
+        .split("\r\n\r\n").toList.map(_.split("\r\n").toList.map(_.toInt)).map(_.sum)
+    
     // Part 1  
-    val answer1: Int = groupedCalories.max
+    val answer1: Int = caloriesPerElf.max
     println(s"Result part 1: ${answer1}")
 
     // Part 2
-    def addMaxThree(l: List[Int], n: Int = 0, total: Int = 0): Int =
-        if (n < 3) addMaxThree(l.diff(List(l.max)), n + 1, total + l.max)
-        else total
-
-    val answer2: Int = addMaxThree(groupedCalories)
+    val answer2: Int = caloriesPerElf.sorted.reverse.take(3).sum
     println(s"Resutl part 2: ${answer2}")
